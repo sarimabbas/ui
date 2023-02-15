c93989eff9e1819f5b418462683d094a87f919da
Sarim Abbas <sarim.stays@gmail.com>
2023-02-14 23:37:10 -0800

    dialog, input, label

---
+++ src/components/label/index.stories.tsx
@@ -0,0 +1,13 @@
+import { Checkbox } from "@/components/checkbox";
+import { Label } from "@/components/label";
+
+export function LabelDemo() {
+  return (
+    <div>
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <Label htmlFor="terms">Accept terms and conditions</Label>
+      </div>
+    </div>
+  );
+}
