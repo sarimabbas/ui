0046cc8ab09994c2eee9af45b6fa5c7fd7c56ebd
Sarim Abbas <sarim.stays@gmail.com>
2023-02-14 22:20:45 -0800

    avatar

---
+++ src/components/avatar/index.stories.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar";
+
+export function AvatarDemo() {
+  return (
+    <Avatar>
+      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  );
+}
+
+export default {
+  component: Avatar,
+};
