77c3046834d42c67207544e3000ba7b971af5288
Sarim Abbas <sarim.stays@gmail.com>
2023-02-16 00:18:18 -0800

    nav menu

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,6 +13,7 @@ export * from "./hover-card";
 export * from "./input";
 export * from "./label";
 export * from "./menubar";
+export * from "./navigation-menu";
 export * from "./popover";
 export * from "./progress";
 export * from "./radio-group";
