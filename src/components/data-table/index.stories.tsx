7eb6eb42fbb1a667452184423a618c6a923907da
Sarim Abbas <sarim.stays@gmail.com>
2023-06-11 19:25:52 -0700

    add data table demo

---
+++ src/components/data-table/index.stories.tsx
@@ -0,0 +1,36 @@
+import { ColumnDef, DataTable } from ".";
+
+type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+const data: Payment[] = [
+  {
+    id: "1",
+    amount: 100,
+    status: "pending",
+    email: "eemail",
+  },
+];
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  },
+];
+
+export function DataTableDemo() {
+  return <DataTable columns={columns} data={data} />;
+}
