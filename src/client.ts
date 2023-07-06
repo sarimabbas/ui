a5acb1c54ca2162fd0f15ccc4bb73c26dcf36bfb
Sarim Abbas <sarim.stays@gmail.com>
2023-07-05 21:20:52 -0700

    add badge

--- src/client.ts
+++ src/client.ts
@@ -6,6 +6,7 @@ export * from "./components/ui/accordion";
 export * from "./components/ui/alert-dialog";
 export * from "./components/ui/aspect-ratio";
 export * from "./components/ui/avatar";
+export * from "./components/ui/badge";
 export * from "./components/ui/button";
 export * from "./components/ui/checkbox";
 export * from "./components/ui/collapsible";
---
+++ src/components/ui/badge.stories.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@/components/ui/badge";
+
+export function BadgeDemo() {
+  return <Badge>Badge</Badge>;
+}
+
+export function BadgeSecondary() {
+  return <Badge variant="secondary">Secondary</Badge>;
+}
+
+export function BadgeOutline() {
+  return <Badge variant="outline">Outline</Badge>;
+}
+
+export function BadgeDestructive() {
+  return <Badge variant="destructive">Destructive</Badge>;
+}
